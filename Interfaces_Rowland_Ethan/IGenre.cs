b7da46ba82d33d5cd35525f2b4ceee225fde8111
generalrowland <ethandale0916@gmail.com>
2023-05-04 13:33:17 -0500

    d

---
+++ Interfaces_Rowland_Ethan/IGenre.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interfaces_Rowland_Ethan
+{
+    interface IGenre
+    {
+        
+        public string Esrb { get; set; }
+
+        public string Genre { get; set; }
+
+        public string Title { get; set; }
+
+        public string Describe();
+
+    }
+}
--- Interfaces_Rowland_Ethan/Program.cs
+++ Interfaces_Rowland_Ethan/Program.cs
@@ -6,7 +6,13 @@ namespace Interfaces_Rowland_Ethan
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Game myGame = new Game("M", "FPS", "FarCry 5");
+            Console.WriteLine(myGame.Describe());
+            myGame.PlayGame();
+
+            Movie myMovie = new Movie("R", "Action/Comedy", "The Hitman's Bodyguard");
+            Console.WriteLine(myMovie.Describe());
+            myMovie.PlayMovie();
         }
     }
 }
